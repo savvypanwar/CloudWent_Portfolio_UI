c916e30c415d9ddb2d3d71f05416a19374af6789
Ritamsundarsandhaki <ritam.sandhaki@rajdigital.com>
2026-07-13 11:29:03 +0530

    Add Google Analytics integration to track page views

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,14 @@
 import type { Metadata } from "next";
+import Script from "next/script";
+
 import "./globals.css";
 import { Providers } from "@/components/Providers";
 import { Toaster } from "react-hot-toast";
 import { siteMetadata } from "@/utils/seo";
 
 export const metadata: Metadata = siteMetadata;
+
+const GA_ID = "G-JHJRXPZM5C";
 
 export default function RootLayout({
   children,
@@ -21,10 +25,31 @@ export default function RootLayout({
       </head>
 
       <body className="flex flex-col min-h-screen bg-background text-foreground antialiased">
+        
+        {/* Google Analytics */}
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+          strategy="afterInteractive"
+        />
+
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){window.dataLayer.push(arguments);}
+
+            gtag('js', new Date());
+
+            gtag('config', '${GA_ID}', {
+              page_path: window.location.pathname,
+            });
+          `}
+        </Script>
+
         <Providers>
           <Toaster position="top-center" />
           <main className="flex-grow">{children}</main>
         </Providers>
+
       </body>
     </html>
   );
